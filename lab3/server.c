26574136f184ced56eb118e6f3da6f8489425013
vighneshhegde <32838128+vighneshhegde@users.noreply.github.com>
2019-02-04 10:43:06 +0530

    Add files via upload

---
+++ lab3/server.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define MAXPENDING 5
+#define BUFFERSIZE 32
+int main ()
+{
+ /*CREATE A TCP SOCKET*/
+ int serverSocket = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP);
+ if (serverSocket < 0) { printf ("Error while server socket creation"); exit (0); } 
+ printf ("Server Socket Created\n"); 
+
+ /*CONSTRUCT LOCAL ADDRESS STRUCTURE*/
+ struct sockaddr_in serverAddress, clientAddress;
+ memset (&serverAddress, 0, sizeof(serverAddress));
+ serverAddress.sin_family = AF_INET;
+ serverAddress.sin_port = htons(12345);
+ serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+ printf ("Server address assigned\n");
+ 
+ int temp = bind(serverSocket, (struct sockaddr*) &serverAddress,  sizeof(serverAddress));
+ if (temp < 0) 
+  { printf ("Error while binding\n"); 
+    exit (0);
+  }
+ printf ("Binding successful\n");
+ int temp1 = listen(serverSocket, MAXPENDING);
+ if (temp1 < 0) 
+  {  printf ("Error in listen"); 
+     exit (0);
+  }
+ printf ("Now Listening\n");
+ char msg[BUFFERSIZE];
+ int clientLength, clientSocket;
+ while(1){ 
+  clientLength = sizeof(clientAddress);
+  clientSocket = accept (serverSocket, (struct sockaddr*) &clientAddress, &clientLength);
+  if (fork()==0){
+	break;
+  }
+  else{
+	close(clientSocket);
+  }
+ }
+	
+  if (clientLength < 0) {printf ("Error in client socket"); exit(0);}
+  printf ("Handling Client %s\n", inet_ntoa(clientAddress.sin_addr));
+  int temp2 = recv(clientSocket, msg, BUFFERSIZE, 0);
+  if (temp2 < 0) 
+    { printf ("problem in temp 2"); 
+      exit (0);
+    }
+  msg[temp2] = '\0';
+  double ans = atof(msg);
+  printf ("%s\n", msg);
+  //printf ("ENTER MESSAGE FOR CLIENT\n");
+  
+  //fgets(msg,BUFFERSIZE,stdin);
+  int res = (ans-(int)ans==0)?ans:((int)ans +1);
+  sprintf(msg,"%d",res);
+  int bytesSent = send (clientSocket,msg,strlen(msg),0);
+  if (bytesSent != strlen(msg)) 
+    { printf ("Error while sending message to client");   
+      exit(0);
+    }
+ close(clientSocket);
+ close (serverSocket);
+}
